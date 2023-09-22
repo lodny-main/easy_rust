3eb0d86b58245f92d69bdc58fa87bc5adde9ca7b
Lodny <drinkjuice@naver.com>
2023-09-22 20:02:20 +0900

    enum

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,22 @@
-use std::mem::size_of_val;
-
-struct SomeStruct;
-#[derive(Debug)]
-struct Colour(u8, u8, u8);
-
-#[derive(Debug)]
-struct Country {
-    population: u32,
-    capital: String,
+enum Number {
+    U32(u32),
+    I32(i32)
 }
+
 
 fn main() {
-    let x = SomeStruct;
-    println!("The size is {}", size_of_val(&x));
-
-    let c = Colour(20, 50, 100);
-    println!("The green value is {}", c.1);
-    println!("The green value is {:?}", c);
-
-    let country = Country {
-        population: 5000_0000,
-        capital: "Seoul".to_string(),
-    };
-    println!("country: {:#?}", country);
+    let vec = vec![get_number(-800), get_number(8)];
+    for item in vec {
+        match item {
+            Number::U32(number) => println!("u32: {}", number),
+            Number::I32(number) => println!("i32: {}", number)
+        }
+    }
 }
 
-
+fn get_number(input: i32) -> Number {
+    match input.is_positive() {
+        true => Number::U32(input as u32),
+        false => Number::I32(input)
+    }
+}
