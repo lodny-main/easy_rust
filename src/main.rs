ee1f870c7435dd5929c11874e529ee8141dc502b
Lodny <drinkjuice@naver.com>
2023-09-22 15:35:04 +0900

    copy type non-copy type ->> clone()

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-fn main() {
-    let mut my_number = 9;
-    let num_ref = &mut my_number;
-    // let num_ref2 = &my_number;   ->> error
-
-    *num_ref = 10;
-
-    println!("Number is now {my_number}");
+fn print_number(number: i32) {
+    println!("{number}");
 }
+
+fn print_string(input: String) {
+    println!("{input}");
+}
+
+fn main() {
+    // Copy type
+    let my_number = 8;
+    print_number(my_number);
+    print_number(my_number);
+
+    // Non Copy type : clone
+    let my_country = "Austria".to_string();
+    print_string(my_country.clone());
+    print_string(my_country);
+}
+
+
