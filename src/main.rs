e9ab3a9b87344e00101399cae70d84b9b5d50bcd
juice <drinkjuice@naver.com>
2023-09-23 08:07:00 +0900

    generics - add Book and impl Display

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use std::fmt::Display;
+use std::fmt::{Display, Formatter};
+
+struct Book;
+
+impl Display for Book {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        println!("()");
+        Ok(())
+    }
+}
 
 fn give_thing<T: Display>(input: T) -> T {
     println!("{}", input);
@@ -8,7 +17,9 @@ fn give_thing<T: Display>(input: T) -> T {
 fn main() {
     let x = give_thing(String::from("Take this thing"));
     let y = give_thing(9);
+    let z = give_thing(Book);
 
     println!("{x}");
     println!("{y}");
+    println!("{z}");
 }
