f936f0008bc6e83a031ea3a2d1ed0b0de768129b
Lodny <drinkjuice@naver.com>
2023-09-22 15:21:44 +0900

    mutable and immutable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut my_number = 9;
+    let num_ref = &mut my_number;
+    // let num_ref2 = &my_number;   ->> error
+
+    *num_ref = 10;
+
+    println!("Number is now {my_number}");
 }
