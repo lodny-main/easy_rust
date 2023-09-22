d1b0b379eb1dd066856cbd6bdf620073e5119bb4
Lodny <drinkjuice@naver.com>
2023-09-22 17:58:53 +0900

    slice

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,7 @@
-fn print_number(number: i32) {
-    println!("{number}");
-}
-
-fn print_string(input: String) {
-    println!("{input}");
-}
-
 fn main() {
-    // Copy type
-    let my_number = 8;
-    print_number(my_number);
-    print_number(my_number);
-
-    // Non Copy type : clone
-    let my_country = "Austria".to_string();
-    print_string(my_country.clone());
-    print_string(my_country);
+    let seasons = ["봄", "여름", "가을", "겨울"];
+    println!("seasons: {:?}", seasons);
+    println!("slice of seasons: {:?}", &seasons[0..2]);
 }
 
 
