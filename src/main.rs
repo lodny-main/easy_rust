75420289d5097eb3c3e468708e3bb33996d43b29
Lodny <drinkjuice@naver.com>
2023-09-22 20:07:46 +0900

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
-enum Number {
-    U32(u32),
-    I32(i32)
-}
-
-
 fn main() {
-    let vec = vec![get_number(-800), get_number(8)];
-    for item in vec {
-        match item {
-            Number::U32(number) => println!("u32: {}", number),
-            Number::I32(number) => println!("i32: {}", number)
+    'first: loop {
+        'second: loop {
+            break 'first;
         }
     }
-}
 
-fn get_number(input: i32) -> Number {
-    match input.is_positive() {
-        true => Number::U32(input as u32),
-        false => Number::I32(input)
+    for number in 0..3 {    // Range
+        println!("number: {}", number);
     }
+
+    let mut counter = 5;
+    let my_number = loop {
+        counter += 1;
+        if counter % 53 == 3 {
+            break counter;
+        }
+    };
+
+    println!("my_number is now: {}", my_number);
 }
