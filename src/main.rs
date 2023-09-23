b2b736aaaaa58f0f2c8062cc05e14e1cc34e8c27
lodny <drinkjuice@naver.com>
2023-09-23 19:26:44 +0900

    Closure - anonymous functions that capture their environment

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,10 @@
-enum LibraryType {
-    City,
-    Country,
-}
-
-struct Library {
-    library_type: LibraryType,
-    books: Vec<String>,
-}
-
-impl Library {
-    fn new() -> Self {
-        Self {
-            library_type: LibraryType::City,
-            books: Vec::new(),
-        }
-    }
-
-    fn add_book(&mut self, book: &str) {
-        self.books.push(book.to_string());
-    }
-}
-
-impl Iterator for Library {
-    type Item = String;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.books.pop()
-    }
-}
+// closure : anonymous functions that capture their environment
+// zero cost abstractions
 
 fn main() {
-    let mut my_library = Library::new();
-    my_library.add_book("The Doom");
-    my_library.add_book("Demian");
-    my_library.add_book("구운몽");
-    my_library.add_book("데이터베이스");
+    let my_number = 10;
 
-    println!("{:?}", my_library.books);
+    let my_closure = |x: i32| println!("{}", x + my_number);
 
-    for book in my_library {
-        println!("{}", book);
-    }
+    my_closure(10);
 }
