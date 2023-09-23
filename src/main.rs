74a9580c5ffe9f14b7fb5b774c1f4236c45ad7a0
juice <drinkjuice@naver.com>
2023-09-23 09:03:23 +0900

    option - expect()

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,8 @@ fn take_fifth(value: Vec<i32>) -> Option<i32> {
 }
 
 fn main() {
-    let vec = vec![1, 2, 1, 2, 1, 2];
-    // match take_fifth(vec) {
-    //     None => println!("There was nothing inside."),
-    //     Some(number) => println!("I got a number: {number}")
-    // }
+    let vec = vec![1, 2];
     let index = take_fifth(vec);
-    if index.is_some() {
-        println!("I got a number: {}", index.unwrap());
-    }
+
+    index.expect("Needed at least five items");
 }
