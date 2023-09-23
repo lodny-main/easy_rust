81b680d1e8014cbf79c371fd2b1e8f9326ff2610
lodny <drinkjuice@naver.com>
2023-09-23 21:56:44 +0900

    Closure - skip() - fold()

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-// .any()
-// .all()
-// .rev()
-// .find()  : I'll try to get it for you
-// .position()  : I'll try to find the position for you
-// .cycle()
+// .skip()
 // .take()
+// .fold() : javascript : reduce()
+// .sum()
 
 fn main() {
-    let even_odd = vec!["even", "odd"];
+    let ten_chars = ('a'..)
+        .skip(10)
+        .take(10)
+        .collect::<Vec<_>>();
+    println!("{ten_chars:?}");
 
-    let even_odd_vec = (0..6)
-        .zip(even_odd.into_iter().cycle())
-        .collect::<Vec<(i32, &str)>>();
-
-    println!("{even_odd_vec:?}");
+    let a_string = "I don't have any dashes in me.";
+    let dashed = a_string
+        .chars()
+        .fold("-".to_string(), |mut string_to_far, next_char| {
+            string_to_far.push(next_char);
+            string_to_far.push('-');
+            string_to_far
+        });
+    println!("{dashed:?}");
 }
