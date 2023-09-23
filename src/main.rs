1b7633ac98e1271fcccc7ad62cb2d4d91fd8a124
lodny <drinkjuice@naver.com>
2023-09-24 07:32:17 +0900

    lifetime - struct and impl struct

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-struct Book<'a> {
+struct Adventurer<'a> {
     name: &'a str,
+    hit_points: u32,
 }
 
-fn returns_reference() -> &'static str {
-    "Juice"
+// implicit == not said
+// elided == not shown
+// impl Adventurer {                // implicit elided lifetime not allowed here
+// impl<'a> Adventurer<'a> {        // older version
+impl Adventurer<'_> {
+    fn take_damage(&mut self) {
+        self.hit_points -= 20;
+        println!("{} has {} hit points left!", self.name, self.hit_points);
+    }
 }
 
 fn main() {
-    let reference = returns_reference();
-    println!("{reference}");
 
-    let my_book = Book {
-        name: "my book"
-    };
 }
