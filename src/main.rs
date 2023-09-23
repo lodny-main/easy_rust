87fac121b50e8efe2b6263c11ad0cb42b77a0899
juice <drinkjuice@naver.com>
2023-09-23 09:19:54 +0900

    Result - Vec<Result<i32, String>>

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
-// Option - Maybe there, maybe not
-// .is_some()
-// .is_none()
-// None.unwrap() ->> panic
-
-// Result - May not work
-// .is_ok()
-// .is_err()
-// Err.unwrap() ->> panic
-
-fn check_error(input: i32) -> Result<(), ()> {
-    if input % 2 == 0 {
-        Ok(())
-    } else {
-        Err(())
+fn check_if_five(number: i32) -> Result<i32, String> {
+    match number {
+        5 => Ok(number),
+        _ => Err("Sorry, the number wasn't five.".to_string())
     }
 }
 
 fn main() {
-    check_error(5).unwrap();
-}
+    let mut result = Vec::new();
 
+    for number in 2..=7 {
+        result.push(check_if_five(number));
+    }
+
+    println!("{:#?}", result);
+}
