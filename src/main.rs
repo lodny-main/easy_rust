34900a2df927a16456b9fd570fc7ef7800a4f4e2
juice <drinkjuice@naver.com>
2023-09-23 09:24:50 +0900

    Result - ParseIntError

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-fn check_if_five(number: i32) -> Result<i32, String> {
-    match number {
-        5 => Ok(number),
-        _ => Err("Sorry, the number wasn't five.".to_string())
-    }
+use std::num::ParseIntError;
+
+fn parse_number(number: &str) -> Result<i32, ParseIntError> {
+    number.parse()
 }
 
 fn main() {
-    let mut result = Vec::new();
+    let mut result = vec![];
+    result.push(parse_number("8"));
+    result.push(parse_number("dasfas"));
+    result.push(parse_number("108"));
 
-    for number in 2..=7 {
-        result.push(check_if_five(number));
+    for number in result {
+        println!("{:?}", number);
     }
-
-    println!("{:#?}", result);
 }
