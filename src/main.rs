00e19c1958d7e6a60655a80048e01c9072b7383d
juice <drinkjuice@naver.com>
2023-09-23 09:11:32 +0900

    Result - using .unwrap()

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 // Option - Maybe there, maybe not
 // .is_some()
 // .is_none()
+// None.unwrap() ->> panic
 
 // Result - May not work
 // .is_ok()
 // .is_err()
+// Err.unwrap() ->> panic
 
 fn check_error(input: i32) -> Result<(), ()> {
     if input % 2 == 0 {
@@ -15,8 +17,6 @@ fn check_error(input: i32) -> Result<(), ()> {
 }
 
 fn main() {
-    match check_error(5) {
-        Ok(_) => println!("It's okay, guys!"),
-        Err(_) => println!("It's an error, guys!")
-    }
+    check_error(5).unwrap();
 }
+
