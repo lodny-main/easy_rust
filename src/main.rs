d776baf11cd3470373ff6676749be4f3bdce7d55
lodny <drinkjuice@naver.com>
2023-09-24 07:13:07 +0900

    lifetime - basic &str

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
-// dbg!()
-// inspect()
+// String
+// &str
+// string literal
+// borrowed str
 
 fn main() {
-    let new_vec = [8, 9, 10];
-
-    let double_vec = new_vec
-        .iter()
-        .inspect(|&first_item| {
-            dbg!(first_item);
-        })
-        .map(|x| x * 2)
-        .inspect(|&first_item| {
-            dbg!(first_item);
-        })
-        .filter(|&num| num > 17)
-        .collect::<Vec<_>>();
-
-    dbg!(double_vec);
+    let my_name = "Juice";
+    let my_string = "Coco".to_string();      // &'static - for the life of the program
+    let my_string_ref = &my_string;         // &str - reference to something else
 }
