2c13a321da5995ba2a35b87a81118d17d2076ebb
lodny <drinkjuice@naver.com>
2023-09-24 08:24:42 +0900

    RefCell - .borrow_mut() - .try_borrow_mut() - drop()

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
-// Cell
-// - not mutable but changeable
-// - not thread safe
-// - small copy types
-// get()
-// set()
+// RefCell
+// - runtime checked borrowing rules
+// - borrow_mut()
+// - try_borrow_mut()
 
-use std::cell::Cell;
+use std::cell::RefCell;
+
+#[derive(Debug)]
+struct User {
+    id: u32,
+    year_registered: u32,
+    username: String,
+    active: RefCell<bool>
+}
 
 fn main() {
-    let my_cell = Cell::new(String::from("I am a String"));
+    let user_1 = User {
+        id: 1,
+        year_registered: 2020,
+        username: "User 1".to_string(),
+        active: RefCell::new(true),
+    };
+    println!("{user_1:?}");
 
-    my_cell.set(String::from("I am a String????"));
-    let my_string = my_cell.get();
+    let mut ref_mut_1 = user_1.active.borrow_mut();     // ->> status : borrowed
+    // let ref_mut_2 = user_1.active.borrow_mut();      // panic
+    println!("{user_1:?}, {ref_mut_1} <-- after borrow_mut()");
+
+    *ref_mut_1 = false;
+    println!("{user_1:?}, {ref_mut_1} <-- after assign false");
+
+    drop(ref_mut_1);
+    println!("{user_1:?} <-- after drop()");
 }
