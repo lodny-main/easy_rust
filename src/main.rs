72d2aa4cca993e94dc4044d58c76795c1fabf1b5
yjahn <drinkjuice93@gmail.com>
2023-09-23 10:39:54 +0900

    HashMap - return tuple from iterator - no order

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-fn main() {
-    let my_vec = vec![2, 3, 4];
+use std::collections::HashMap;
 
-    for index in 0..10 {
-        if let Some(number) = my_vec.get(index) {
-            println!("The number is: {}", number);
-        }
+struct City {
+    name: String,
+    population: HashMap<u32, u32>   // year + population
+}
+
+fn main() {
+    let mut tallinn = City {
+        name: "Tallinn".to_string(),
+        population: HashMap::new(),
+    };
+
+    tallinn.population.insert(1372, 3250);
+    tallinn.population.insert(1851, 2_4000);
+    tallinn.population.insert(2020, 43_7619);
+
+    for (year, population) in tallinn.population {
+        println!("In the year {} the population was {}", year, population);
     }
 }
