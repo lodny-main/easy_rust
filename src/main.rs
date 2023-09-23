d07ef975bc23140ebc8a48af13d6afbae48482d8
yjahn <drinkjuice93@gmail.com>
2023-09-23 11:07:10 +0900

    HashMap - .entry() - .and_modify(||) - .or_insert()

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
 use std::collections::HashMap;
 
 fn main() {
+    let book_vec = vec![
+        "book 1",
+        "book 2",
+        "book 3",
+        "book 4",
+        "book 3",
+        "book 3",
+        "book 1",
+    ];
+
     let mut book_hashmap = HashMap::new();
 
-    book_hashmap.insert(1, "L'Allemagne Moderne");
+    for book in book_vec {
+        // let number_of_books = book_hashmap.entry(book).or_insert(0);
+        // *number_of_books += 1;
 
-    if book_hashmap.get(&1).is_none() {
-        book_hashmap.insert(1, "Le Petit Prince");
-    } else {
-        println!("Already got a book");
+        book_hashmap
+            .entry(book)
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
     }
 
-    if let Some(book_name) = book_hashmap.get(&1) {
-        println!("Already got a book: {}", book_name);
-    } else {
-        book_hashmap.insert(1, "Le Petit Prince");
+    for (book, number) in book_hashmap {
+        println!("{}: {} copies", book, number);
     }
 }
