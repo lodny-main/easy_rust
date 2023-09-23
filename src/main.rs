c43ae44cc7c0d7c670390c94e79197cfec7c8929
juice <drinkjuice@naver.com>
2023-09-23 10:06:16 +0900

    Option - Vec::get()

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-use std::num::ParseIntError;
-
-fn parse_number(number: &str) -> Result<i32, ParseIntError> {
-    number.parse()
-}
-
 fn main() {
-    let mut result = vec![];
-    result.push(parse_number("8"));
-    result.push(parse_number("dasfas"));
-    result.push(parse_number("108"));
+    let my_vec = vec![2, 3, 4];
+    let one = my_vec.get(0);
+    let two = my_vec.get(10);
 
-    for number in result {
-        println!("{:?}", number);
-    }
+    println!("{:?}, {:?}", one, two);
 }
