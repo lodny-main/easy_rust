a6f280c1f0f908b8167af072aef40e6f8f791c1b
yjahn <drinkjuice93@gmail.com>
2023-09-23 11:13:41 +0900

    HashMap - tuple to map

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,25 @@
 use std::collections::HashMap;
 
 fn main() {
-    let book_vec = vec![
-        "book 1",
-        "book 2",
-        "book 3",
-        "book 4",
-        "book 3",
-        "book 3",
-        "book 1",
+    let data = vec![
+        ("male", 9),
+        ("female", 5),
+        ("male", 0),
+        ("female", 6),
+        ("female", 5),
+        ("male", 10),
     ];
 
-    let mut book_hashmap = HashMap::new();
+    let mut survey_hash = HashMap::new();
 
-    for book in book_vec {
-        // let number_of_books = book_hashmap.entry(book).or_insert(0);
-        // *number_of_books += 1;
-
-        book_hashmap
-            .entry(book)
-            .and_modify(|counter| *counter += 1)
-            .or_insert(1);
+    for (gender, number) in data {
+        survey_hash
+            .entry(gender)
+            .or_insert(Vec::new())
+            .push(number);
     }
 
-    for (book, number) in book_hashmap {
-        println!("{}: {} copies", book, number);
+    for (gender, numbers) in survey_hash {
+        println!("{:?}, {:?}", gender, numbers);
     }
 }
