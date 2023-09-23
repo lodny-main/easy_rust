969c6496f66560a4f1e7a0be080c56936fb7b816
lodny <drinkjuice@naver.com>
2023-09-24 07:22:29 +0900

    lifetime - basic &str 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-// String
-// &str
-// string literal
-// borrowed str
+struct Book<'a> {
+    name: &'a str,
+}
+
+fn returns_reference() -> &'static str {
+    "Juice"
+}
 
 fn main() {
-    let my_name = "Juice";
-    let my_string = "Coco".to_string();      // &'static - for the life of the program
-    let my_string_ref = &my_string;         // &str - reference to something else
+    let reference = returns_reference();
+    println!("{reference}");
+
+    let my_book = Book {
+        name: "my book"
+    };
 }
