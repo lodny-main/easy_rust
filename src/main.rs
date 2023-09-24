fe374837fee5e7ef0ac640e247967c95ca2303f4
lodny <drinkjuice@naver.com>
2023-09-24 19:53:20 +0900

    Box - owned data on the heap - size_of_val()

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-// Mutex and RwLock(Read Write)
+// Box : owned data on the heap
 
-use std::sync::RwLock;
+use std::mem::size_of_val;
+
+struct SomeStruct {
+    name: String,
+    number: u8,
+    data: Box<[u32; 10000]>,
+}
 
 fn main() {
-    let my_rwlock = RwLock::new(5);
-    println!("{my_rwlock:?}");
+    let my_struct = SomeStruct {
+        name: "Hi, there".to_string(),
+        number: 0,
+        data: Box::new([9; 10000]),
+    };
 
-    let read1 = my_rwlock.read().unwrap();      // lock
-    let read2 = my_rwlock.read().unwrap();      // lock
-    println!("{read1}, {read2}");
+    let my_box = Box::new(9);
 
-    // like db ?
-    let read3 = my_rwlock.read().unwrap();      // lock
-    let write1 = my_rwlock.write().unwrap();      // lock
-    println!("{read3}, {write1}");
+    println!("{my_box}");
+    println!("size of SomeStruct: {}", size_of_val(&my_struct));
 }
