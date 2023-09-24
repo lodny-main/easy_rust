22ad1aa4bcb8e69ed10ef28e0b3e1178859d72ff
lodny <drinkjuice@naver.com>
2023-09-24 09:08:30 +0900

    RefCell - using dereference - match

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,12 @@
-// RefCell
-// - runtime checked borrowing rules
-// - borrow_mut()
-// - try_borrow_mut()
-
 use std::cell::RefCell;
 
-#[derive(Debug)]
-struct User {
-    id: u32,
-    year_registered: u32,
-    username: String,
-    active: RefCell<bool>
-}
-
 fn main() {
-    let user_1 = User {
-        id: 1,
-        year_registered: 2020,
-        username: "User 1".to_string(),
-        active: RefCell::new(true),
-    };
-    println!("{user_1:?}");
-
-    let mut ref_mut_1 = user_1.active.borrow_mut();     // ->> status : borrowed
-    // let ref_mut_2 = user_1.active.borrow_mut();      // panic
-    println!("{user_1:?}, {ref_mut_1} <-- after borrow_mut()");
-
-    *ref_mut_1 = false;
-    println!("{user_1:?}, {ref_mut_1} <-- after assign false");
-
-    drop(ref_mut_1);
-    println!("{user_1:?} <-- after drop()");
+    let my_cell = RefCell::new(String::from("I am a String"));
+    println!("{my_cell:?}");
+    // *my_cell.borrow_mut() = String::from("I am not a String");
+    match my_cell.try_borrow_mut() {
+        Ok(mut r) => *r = String::from("I am not a String"),
+        Err(e) => println!("We got an error: {e}")
+    }
+    println!("{my_cell:?}");
 }
