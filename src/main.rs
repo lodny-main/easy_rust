b03b9fb8e3402542b94c906498a9a9211c18eb79
lodny <drinkjuice@naver.com>
2023-09-24 20:04:26 +0900

    Box - recursive type using enum

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
-// Box : owned data on the heap
+// recursive types
 
-use std::mem::size_of_val;
+#[derive(Debug)]
+struct List {
+    // content: List,      // --> error: recursive type `List` has infinite size
+    content: Box<List>,
+}
 
-struct SomeStruct {
-    name: String,
-    number: u8,
-    data: Box<[u32; 10000]>,
+#[derive(Debug)]
+enum ListEnum {
+    Content(i32, Box<ListEnum>),
+    NoContent,
 }
 
 fn main() {
-    let my_struct = SomeStruct {
-        name: "Hi, there".to_string(),
-        number: 0,
-        data: Box::new([9; 10000]),
-    };
+    let list_enum = ListEnum::Content(213, Box::new(ListEnum::NoContent));
 
-    let my_box = Box::new(9);
-
-    println!("{my_box}");
-    println!("size of SomeStruct: {}", size_of_val(&my_struct));
+    println!("{list_enum:?}");
 }
