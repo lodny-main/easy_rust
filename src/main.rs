912b2b757c9cb45fb031c308c14e4896c332a5b1
lodny <drinkjuice@naver.com>
2023-09-24 15:13:54 +0900

    Thread - thread::spwan() - handle.join()

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-// todo!() macro
-// alias = different name
-
-use std::iter::{Skip, Take};
-use std::vec::IntoIter;
-
-type SkipAndTake = Take<Skip<IntoIter<char>>>;
-struct  MyOtherString(String);
-
-fn skip_five_take_five(input: Vec<char>) -> SkipAndTake {
-    input
-        .into_iter()
-        .skip(5)
-        .take(5)
-}
+use std::thread;
 
 fn main() {
+    let mut join_vec = vec![];
+    for _ in 1..10 {
+        let handle = thread::spawn(|| {
+            println!("I am printing something");
+        });
+        join_vec.push(handle);
+    }
 
+    // for handle in join_vec {
+    //     handle.join().unwrap();
+    // }
+    join_vec
+        .into_iter()
+        .for_each(|handle| handle.join().unwrap())
 }
-
