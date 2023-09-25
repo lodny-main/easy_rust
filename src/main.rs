e68ea9b71c3fd346264cd6c9c779c1844dcc426d
lodny <drinkjuice@naver.com>
2023-09-25 22:08:41 +0900

    Default - .default()

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,5 @@
-// Cow = Clone on Write
-
-// + Add
-// - Sub
-// += AddAssign
-
-use std::ops::Add;
-
-
-#[derive(Debug)]
-struct Country {
-    name: String,
-    population: u32,
-    gdp: u32,
-}
-
-impl Add for Country {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self {
-        Self {
-            name: format!("{} and {}", self.name, other.name),
-            population: self.population + other.population,
-            gdp: self.gdp + other.gdp,
-        }
-    }
-}
-
-impl Country {
-    fn new(name: &str, population: u32, gdp: u32) -> Self {
-        Self {
-            name: String::from(name),
-            population,
-            gdp,
-        }
-    }
-}
+// Default and the builder pattern
 
 fn main() {
-    let nauru = Country::new("Nauru", 1_0670, 1_6000_0000);
-    let vanuatu = Country::new("Vanuatu", 30_7815, 8_2000_0000);
-    let micronesia = Country::new("Micronesia", 10_4468, 3_6700_0000);
-
-    println!("Nauru + Vanuatu + Micronesia = {:?}", nauru + vanuatu + micronesia);
+    println!("{}, {}, {}", i32::default(), String::default(), f32::default());
 }
