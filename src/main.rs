0b4b94f6fc4b437c453516f344e7cf6058684ab9
lodny <drinkjuice@naver.com>
2023-09-26 06:52:49 +0900

    Any - type_name()

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,18 @@
-#![allow(dead_code)]
-#![warn(
-    clippy::pedantic,
-    // clippy::nursery,
-    // clippy::cargo,
-)]
+// java: typeof
+// downcasting -> dynamically making concrete
+// &dyn Any
 
-const OKAY_CHARACTERS: &str = "1324567890+- ";
+use std::any::{Any, type_name};
 
-fn math(input: &str) -> i32 {
-    if !input.chars().all(|character| OKAY_CHARACTERS.contains(character)) {
-        panic!("Please only input numbers, +-, or spaces");
-    }
+struct MyType;
 
-    // let input = input.replace(" ", "");
-    let input = input
-        .replace(" ", "")
-        .trim_end_matches(|x| "+-".contains(x))
-        .chars()
-        // .filter(|x| *x != ' ')
-        .collect::<String>();
-    println!("{input:?}");
-    9
-}
-
-// replace : remove blank
-// remove after last number
-
-
-// Test-Driven development
-#[cfg(test)]
-mod tests {
-    use super::*;   // can use outside of mod tests
-
-    #[test]
-    fn one_plus_one_is_two() {
-        assert_eq!(math("1 + 1 +-++-   "), 2);
-    }
-
-    #[test]
-    fn one_minus_minus_one_is_two() {
-        assert_eq!(math("1 - -1"), 2);
-    }
-
-    #[test]
-    #[should_panic]
-    fn panics_when_characters_not_right() {
-        math("7 + please add seven");
-    }
-
+fn get_type_name<T: Any>(_: T) {
+    let my_type = type_name::<T>();
+    println!("{my_type}");
 }
 
 fn main() {
-
+    get_type_name(8);
+    get_type_name(vec![8]);
+    get_type_name(MyType);
 }
