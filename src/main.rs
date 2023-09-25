a1332905c2759e58c58a4b7c627f9c6a18677291
lodny <drinkjuice@naver.com>
2023-09-25 22:02:04 +0900

    Cow - Add trait

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,45 @@
-use std::borrow::Cow;
+// Cow = Clone on Write
+
+// + Add
+// - Sub
+// += AddAssign
+
+use std::ops::Add;
+
 
 #[derive(Debug)]
-struct User<'a> {
-    name: Cow<'a, str>,
+struct Country {
+    name: String,
+    population: u32,
+    gdp: u32,
 }
 
-impl<'a> User<'a> {
-    fn is_borrowed(&self) {
-        match &self.name {
-            Cow::Borrowed(name) => println!("It's borrowed: {name}"),
-            Cow::Owned(name) => println!("It's owned: {name}")
+impl Add for Country {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            name: format!("{} and {}", self.name, other.name),
+            population: self.population + other.population,
+            gdp: self.gdp + other.gdp,
         }
     }
 }
 
-
-fn main() {
-    let mut user_1 = User {
-        name: "User 1".into(),
-    };
-
-    let user_2 = User {
-        name: "User 2".to_string().into(),
-    };
-
-    user_1.is_borrowed();
-    user_1.name.to_mut().push_str("~~~");
-    user_1.is_borrowed();
+impl Country {
+    fn new(name: &str, population: u32, gdp: u32) -> Self {
+        Self {
+            name: String::from(name),
+            population,
+            gdp,
+        }
+    }
 }
 
+fn main() {
+    let nauru = Country::new("Nauru", 1_0670, 1_6000_0000);
+    let vanuatu = Country::new("Vanuatu", 30_7815, 8_2000_0000);
+    let micronesia = Country::new("Micronesia", 10_4468, 3_6700_0000);
+
+    println!("Nauru + Vanuatu + Micronesia = {:?}", nauru + vanuatu + micronesia);
+}
