b27ac3c62c1284c93e96e92bab7d9dd5795070c0
lodny <drinkjuice@naver.com>
2023-09-26 21:55:52 +0900

    channel - Receiver::try_recv() - Receiver::recv_timeout()

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@
 
 use std::sync::mpsc::channel;
 use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
+
+fn sleepy(time: u64) {
+    sleep(Duration::from_millis(time));
+}
 
 fn main() {
     let (tx, rx) = channel();
@@ -10,13 +16,19 @@ fn main() {
     let s1 = tx.clone();
     let s2 = tx.clone();
     thread::spawn(move || {
+        sleepy(1000);
         s1.send(9).unwrap();
     });
 
     thread::spawn(move || {
+        sleepy(1000);
         s2.send(3).unwrap();
     });
 
-    println!("received: {}", rx.recv().unwrap());
-    println!("received: {}", rx.recv().unwrap());
+    // println!("received: {}", rx.recv().unwrap());   // blocking
+    // println!("received: {}", rx.recv().unwrap());
+
+    // println!("received: {:?}", rx.try_recv());
+    // println!("received: {:?}", rx.try_recv());
+    println!("received: {:?}", rx.recv_timeout(Duration::from_millis(500)));
 }
