bdd26b512147ebc5bd5056a83b2c436e6565aa1a
lodny <drinkjuice@naver.com>
2023-09-26 21:30:27 +0900

    panic - take_hook()

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 // panic
 
-use std::panic::set_hook;
+use std::panic::{set_hook, take_hook};
 
 fn main() {
-    let important_code = 400;
+    let mut important_code = 400;
 
     set_hook(Box::new(|panic_info| {
         println!("Didn't get a 200 code yet");
         println!("Panic info: {:?}", panic_info.payload().downcast_ref::<&str>());
     }));
 
-    panic!("Oh the humanity!");
+    let my_number = "8876a".parse::<i32>().unwrap();
+    important_code = 200;
+    let _ = take_hook();
+    let other_number = "dsfa2133123".parse::<i32>().unwrap();
 }
